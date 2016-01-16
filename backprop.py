1fff9a78abf0eacf7ab254797f1c0a0d67686bda
craignicol <craig.nicol@gmail.com>
2016-01-16 23:15:13 +0000

    Add 2nd line of inner nodes to make it deep

--- backprop.py
+++ backprop.py
@@ -14,11 +14,12 @@ Y = np.array([[1],
 class backprop():
     weights1 = []
     weights2 = []
+    weights3 = None
     
     training_cycles = 60000
     max_training_examples = 2
     
-    def __init__(self, input_size, output_size, hidden_nodes, seed = 1, training_cycles = 60000, max_training_examples = 0):
+    def __init__(self, input_size, output_size, hidden_nodes_1, hidden_nodes_2 = 0, seed = 1, training_cycles = 60000, max_training_examples = 0):
         np.random.seed(seed)
         self.training_cycles = training_cycles
         self.max_training_examples = max_training_examples
@@ -26,8 +27,12 @@ class backprop():
         # Let's define 2 hidden layers
         # input (X) * weights1 * weights2 = output (Y)
         # X contains 783 columns, Y is a single value, scaled from 0 to 9 from the standard 0 to 1
-        self.weights1 = np.random.random((input_size, hidden_nodes))
-        self.weights2 = np.random.random((hidden_nodes, output_size))
+        self.weights1 = np.random.random((input_size, hidden_nodes_1))
+        if hidden_nodes_2 == 0:
+            self.weights2 = np.random.random((hidden_nodes_1, output_size))
+        else:
+            self.weights2 = np.random.random((hidden_nodes_1, hidden_nodes_2))
+            self.weights3 = np.random.random((hidden_nodes_2, output_size))
     
     def nonlin(self,x,deriv=False):
         if(deriv==True):
@@ -40,14 +45,25 @@ class backprop():
         l0 = X
         l1 = self.nonlin(np.dot(l0,self.weights1))
         l2 = self.nonlin(np.dot(l1,self.weights2))
+        if self.weights3 is not None:
+            l3 = self.nonlin(np.dot(l2,self.weights3))
+            return(l3, l2, l1)
         
-        return (l2, l1)
+        return (l2, l2, l1)
     
     def train_once(self, l0, Y):
-        (l2, l1) = self.propagate(l0)
+        (l3, l2, l1) = self.propagate(l0)
         
-        # level 2 learning
-        l2_error = Y - l2
+        # level 3 learning
+        if self.weights3 is not None:
+            l3_error = Y - l3
+            l3_delta = l3_error * self.nonlin(l3,deriv=True)
+            
+            l2_error = l3_delta.dot(self.weights3.T)
+        else:
+            # level 2 learning
+            l2_error = Y - l2
+            
         l2_delta = l2_error * self.nonlin(l2,deriv=True)
         
         # level 1 learning
@@ -55,6 +71,8 @@ class backprop():
         l1_delta = l1_error * self.nonlin(l1,deriv=True)
         
         # update weights
+        if self.weights3 is not None:
+            self.weights3 += l2.T.dot(l3_delta)
         self.weights2 += l1.T.dot(l2_delta)
         self.weights1 += l0.T.dot(l1_delta)
         
@@ -82,7 +100,7 @@ class backprop():
     def classify(self, X):
         return self.propagate(X)[0]
 
-bp = backprop(len(X[0]), len(Y[0]), 4, max_training_examples = 10)
+bp = backprop(len(X[0]), len(Y[0]), 4, hidden_nodes_2 = 4, max_training_examples = 10)
 bp.train(X, Y)
 
 print ("Output (line) - should be 0 : {:1.3f}".format(bp.classify([0.5] * 1000)[0]))
