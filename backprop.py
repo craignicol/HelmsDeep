05b881082d1d7117406a8f938ab0548a0687fe33
craignicol <craig.nicol@gmail.com>
2016-01-16 19:32:33 +0000

    Convert to class and upgrade to Py3k

--- backprop.py
+++ backprop.py
@@ -2,62 +2,77 @@ import numpy as np
 from math import sin, cos
 
 def nonlin(x,deriv=False):
-	if(deriv==True):
-	    return x*(1-x)
-
-	return 1/(1+np.exp(-x))
+    if(deriv==True):
+        return x*(1-x)
+    
+    return 1/(1+np.exp(-x))
     
 X = np.array([[sin(x/10) for x in range(1000)],
             [1] * (1000),
             [cos(x/10) for x in range(1000)],
             [0] * (1000)])
                 
-y = np.array([[1],
-			[0],
-			[1],
-			[0]])
+Y = np.array([[1],
+            [0],
+            [1],
+            [0]])
 
-np.random.seed(1)
-
-# randomly initialize our weights with mean 0
-syn0 = 2*np.random.random((1000,4)) - 1
-syn1 = 2*np.random.random((4,1)) - 1
-
-for j in xrange(60000):
-
-	# Feed forward through layers 0, 1, and 2
-    l0 = X
-    l1 = nonlin(np.dot(l0,syn0))
-    l2 = nonlin(np.dot(l1,syn1))
-
-    # how much did we miss the target value?
-    l2_error = y - l2
+class backprop():
+    weights1 = []
+    weights2 = []
     
-    if (j% 10000) == 0:
-        print "Error:" + str(np.mean(np.abs(l2_error)))
+    def __init__(self, input_size, output_size, hidden_nodes, seed = 1):
+        np.random.seed(seed)
+    
+        # Let's define 2 hidden layers
+        # input (X) * weights1 * weights2 = output (Y)
+        # X contains 783 columns, Y is a single value, scaled from 0 to 9 from the standard 0 to 1
+        self.weights1 = np.random.random((input_size, hidden_nodes))
+        self.weights2 = np.random.random((hidden_nodes, output_size))
+    
+    def nonlin(self,x,deriv=False):
+        if(deriv==True):
+            return x*(1-x)
+        return 1/(1+np.exp(-x))
+    
+    def propagate(self, X):
+        # input (X) * weights1 * weights2 = output (Y)
+        # Feed forward through layers 0, 1, and 2
+        l0 = X
+        l1 = nonlin(np.dot(l0,self.weights1))
+        l2 = nonlin(np.dot(l1,self.weights2))
         
-    # in what direction is the target value?
-    # were we really sure? if so, don't change too much.
-    l2_delta = l2_error*nonlin(l2,deriv=True)
-
-    # how much did each l1 value contribute to the l2 error (according to the weights)?
-    l1_error = l2_delta.dot(syn1.T)
+        return (l2, l1)
     
-    # in what direction is the target l1?
-    # were we really sure? if so, don't change too much.
-    l1_delta = l1_error * nonlin(l1,deriv=True)
-
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
-
-def classify(new_values):
-    # Feed forward through layers 0, 1, and 2
-    l0 = new_values
-    l1 = nonlin(np.dot(l0,syn0))
-    l2 = nonlin(np.dot(l1,syn1))
+    def train_once(self, l0, Y):
+        (l2, l1) = self.propagate(l0)
+        
+        # level 2 learning
+        l2_error = Y - l2
+        l2_delta = l2_error*nonlin(l2,deriv=True)
+        
+        # level 1 learning
+        l1_error = l2_delta.dot(self.weights2.T)
+        l1_delta = l1_error * nonlin(l1,deriv=True)
+        
+        # update weights
+        self.weights2 += l1.T.dot(l2_delta)
+        self.weights1 += l0.T.dot(l1_delta)
+        
+        return l2_error
     
-    return l2
+    def train(self, inputs, outputs):
+        for x in range(60000):
+            error = self.train_once(inputs, outputs)
+            if (x % 10000) == 0:
+                print ("Error: " + str(np.mean(np.abs(error))))
     
-print "Output (line) :", classify([0.5] * 1000)
-print "Output (sawtooth) :", classify([(x / 10.0) % 1 for x in range(1000)])
-print "Output (offset sin) :", classify([sin((x+10)/10) for x in range(1000)])
+    def classify(self, X):
+        return self.propagate(X)[0]
+
+bp = backprop(len(X[0]), len(Y[0]), 4)
+bp.train(X, Y)
+
+print ("Output (line) - should be 0 : {0}".format(bp.classify([0.5] * 1000)))
+print ("Output (sawtooth) - should be 0 : {0}".format(bp.classify([(x / 10.0) % 1 for x in range(1000)])))
+print ("Output (offset sin) - should be 1 : {0}".format(bp.classify([sin((x+10)/10) for x in range(1000)])))
